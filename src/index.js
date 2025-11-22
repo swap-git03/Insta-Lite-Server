05842b18b133d32bfc7c947781a6b70e841a4c06
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-11-23 00:03:54 +0530

    done!!

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+const path = require("path");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // DB Connection
 connectDB();
