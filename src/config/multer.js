206e9cb16ab144c7b920b8e29e9cea36def08cc7
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-12-03 19:03:07 +0530

    /

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,25 +1,4 @@
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("cloudinary").v2;
+const { storage } = require("../utils/cloudinary");
 
-/* ==== CLOUDINARY CONFIG ==== */
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET
-});
-
-/* ==== CLOUDINARY STORAGE ==== */
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "swapinsta",          // Cloud folder name
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
-    transformation: [{ width: 1080, crop: "limit" }] // Optional resize
-  }
-});
-
-/* ==== MULTER UPLOAD ==== */
-const upload = multer({ storage });
-
-module.exports = upload;
+module.exports = multer({ storage });
