e9ac0d599e7f1556edc1322a0595afb90ce7cb8f
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-12-21 19:55:30 +0530

    /

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
 });
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
   console.log(`🚀 Local server running on port ${PORT}`)
 );
